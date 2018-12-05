2968a2801911c32347d895011624676fe9db0b80
BasKalkman <b_kalkman@hotmail.com>
2018-12-05 14:11:33 +0100

    day 2 part 2

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -44,26 +44,26 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
 fs.readFile('./input.txt', 'utf8', (err, data2) => {
   var arr2 = data2.split('\r\n');
 
-  let i = 0;
-  let found = false;
+  arr2.forEach(item => {
+    let a = '';
+    let b = '';
 
-  while (found === false) {
-    let countSame = 0;
-    let testBase = arr2[i].split('');
-    // Test Base ID to remaining IDs, letter by letter. Increasing countSame if the same letter is in the same position
-    for (let j = i + 1; j < arr2.length; j++) {
-      let testIterable = arr2[j].split('');
-      for (let k = 0; k < testBase.length; k++) {
-        if (testBase[k] === testIterable[k]) {
-          countSame++;
+    arr2.forEach(check => {
+      let countDiff = 0;
+      for (let i = 0; i < item.length; i++) {
+        if (item[i] !== check[i]) {
+          countDiff++;
         }
       }
-      if (countSame === 1) {
-        console.log('A: ', arr2[i]);
-        console.log('B: ', arr2[j]);
-        found = true;
+      if (countDiff === 1) {
+        a = item;
+        b = check;
       }
+    });
+
+    if (a.length > 0) {
+      console.log(a);
+      console.log(b);
     }
-    i++;
-  }
+  });
 });
