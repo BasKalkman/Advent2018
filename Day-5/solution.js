f464f78d9de8ab264a7b9d5ed0aeba178a9f83b1
Bas K <39069678+Basje12@users.noreply.github.com>
2018-12-08 15:23:43 +0100

    Day 5 Part 2

--- Day-5/solution.js
+++ Day-5/solution.js
@@ -5,15 +5,15 @@ const input = fs.readFileSync('./input.txt', 'utf8').split('');
 const checkLowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const checkUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function runCheck() {
-  for (let i = 0; i < input.length; i++) {
+function runCheck(arr) {
+  for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < checkLowerCase.length; j++) {
-      if (input[i] === checkLowerCase[j] && input[i + 1] === checkUpperCase[j]) {
-        input.splice(i, 2);
+      if (arr[i] === checkLowerCase[j] && arr[i + 1] === checkUpperCase[j]) {
+        arr.splice(i, 2);
         removalsThisRound++;
       }
-      if (input[i] === checkUpperCase[j] && input[i + 1] === checkLowerCase[j]) {
-        input.splice(i, 2);
+      if (arr[i] === checkUpperCase[j] && arr[i + 1] === checkLowerCase[j]) {
+        arr.splice(i, 2);
         removalsThisRound++;
       }
     }
@@ -23,15 +23,44 @@ function runCheck() {
 let noMoreOptions = false;
 let removalsThisRound = 0;
 
-while (noMoreOptions === false) {
-  removalsThisRound = 0;
+function part1() {
+  while (noMoreOptions === false) {
+    removalsThisRound = 0;
 
-  runCheck();
+    runCheck(input);
 
-  if (removalsThisRound === 0) {
-    noMoreOptions = true;
-    console.log(input.length);
+    if (removalsThisRound === 0) {
+      noMoreOptions = true;
+      console.log(input.length);
+    }
   }
 }
 
+// part1();
+
 // PART 2
+function part2() {
+  let counts = [];
+
+  for (let i = 0; i < checkLowerCase.length; i++) {
+    let temp = input.filter(item => {
+      if (item !== checkLowerCase[i] && item !== checkUpperCase[i]) {
+        return true;
+      }
+    });
+    noMoreOptions = false;
+
+    while (noMoreOptions === false) {
+      removalsThisRound = 0;
+
+      runCheck(temp);
+
+      if (removalsThisRound === 0) {
+        noMoreOptions = true;
+        counts.push(temp.length);
+      }
+    }
+  }
+  console.log(counts);
+}
+part2();
