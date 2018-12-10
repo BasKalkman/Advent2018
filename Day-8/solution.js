4d9fa95827f75a7ae4b5d13850536f3e29413500
Bas K <39069678+Basje12@users.noreply.github.com>
2018-12-10 22:45:03 +0100

    Day 8 Part 2

--- Day-8/solution.js
+++ Day-8/solution.js
@@ -1,27 +1,41 @@
 const fs = require('fs');
 
-// 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
-
-// if first > 0 - has children
-// second number is # metadata
-
 const data = fs
   .readFileSync('./input.txt', { encoding: 'utf8' })
   .split(' ')
   .map(item => parseInt(item));
 
+// Write values for part 1 to array
 let metadata = [];
 
 function parseTree() {
   let obj = {};
+  // Set the header for the node
   obj.header = data.splice(0, 2);
+  // Write children to array recursively, if there are any
   obj.child = [];
   for (let i = 0; i < obj.header[0]; i++) {
     obj.child.push(parseTree());
   }
+  // Set metadata for node
   obj.metadata = data.splice(0, obj.header[1]);
 
+  // Part 1: Write metadata values to array
   obj.metadata.forEach(item => metadata.push(item));
+
+  // Part 2: Assign a value to the node, based on children
+  obj.value = 0;
+  if (obj.child.length === 0) {
+    obj.value = obj.metadata.reduce((a, c) => {
+      return a + c;
+    });
+  } else {
+    obj.metadata.forEach(index => {
+      if (obj.child[index - 1]) {
+        obj.value += obj.child[index - 1].value;
+      }
+    });
+  }
 
   return obj;
 }
@@ -32,4 +46,5 @@ let result = metadata.reduce((acc, current) => {
   return acc + current;
 });
 
-console.log(result);
+console.log('Result for part 1: ', result);
+console.log('Result for part 2: ', tree.value);
