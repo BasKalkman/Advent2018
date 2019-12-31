a0f937e25458ab75a509455459cf77c6b497132d
Bas K <39069678+Basje12@users.noreply.github.com>
2019-12-31 13:01:12 +0100

    Day 2 - Python

--- Day-2/solution.py
+++ Day-2/solution.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import difflib
 
 data = open("./input.txt").read().splitlines()
 
@@ -15,3 +16,14 @@ for str in data:
 print(len(twos) * len(threes))
 
 # PART 2
+for i in range(0, len(data)):
+    first_string = data[i]
+    for j in range(i + 1, len(data)):
+        second_string = data[j]
+        diff = 0
+        for idx in range(len(first_string)):
+            if first_string[idx] != second_string[idx]:
+                diff += 1
+        if diff == 1:
+            print(first_string)
+            print(second_string)
